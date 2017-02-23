d7a13cdbae230eba22689c0e5b8e4e098f36384f
Prachi Utkhede <prachs1987@gmail.com>
2017-02-23 00:13:12 -0800

    Fixing formatting issues

--- src/com/prachi/algorithms/UniqueString.java
+++ src/com/prachi/algorithms/UniqueString.java
@@ -2,24 +2,24 @@ package com.prachi.algorithms;
 
 public class UniqueString {
 
-	public static boolean isUnique(String s) {
-		boolean[] uniquechar = new boolean[26];		
-		
-		s = s.toLowerCase();
-		for (int i = 0; i < s.length(); i++) {
-			int val = s.charAt(i) - 'a';
-			
-			if (uniquechar[val]) {
-				return false;
-			}
-			
-			uniquechar[val] = true;
-		}
-		return true;
-	}
+    public static boolean isUnique(String s) {
+        boolean[] uniquechar = new boolean[26];		
 
-	public static void main(String[] args) {
-		System.out.println("Is unique \"BACKUPp\": " + UniqueString.isUnique("BACKUPp"));
-		System.out.println("Is unique \"Phone\": " + UniqueString.isUnique("Phone"));
-	}
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - 'a';
+
+            if (uniquechar[val]) {
+                return false;
+            }
+
+            uniquechar[val] = true;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is unique \"BACKUPp\": " + UniqueString.isUnique("BACKUPp"));
+        System.out.println("Is unique \"Phone\": " + UniqueString.isUnique("Phone"));
+    }
 }
